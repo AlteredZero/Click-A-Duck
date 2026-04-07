e6921c629a3e4b73e731e97b6557ef786929b611
alteredzero <topzgameplayer@gmail.com>
2026-04-07 12:24:12 -0400

    fixed position for buy and sell buttons

--- duck_company_stock.py
+++ duck_company_stock.py
@@ -61,38 +61,40 @@ def open_duck_company_stock_frame(screen, fonts, game_data, draw_animated_text):
         "duck_company_stock_title"
     )
 
+    draw_animated_text(
+        screen,
+        f"Price per share: ",
+        fonts["small"],
+        (255, 255, 255),
+        (menu_rect.centerx, menu_rect.top + sy(60)),
+        "duck_company_stock_title"
+    )
+
     buy_center_y = menu_rect.bottom - sy(80) + fonts["small"].get_height() // 2
     buy_rect = pygame.Rect(menu_rect.centerx - sx(200), buy_center_y - sy(20), sx(200), sy(40))
 
-    button_text_buy = f"BUY"
+    btn_w, btn_h = sx(100), sy(40)
+    padding = sx(20)
+
+    sell_rect = pygame.Rect(menu_rect.right - btn_w - padding, menu_rect.bottom - btn_h - padding, btn_w, btn_h)
+    buy_rect = pygame.Rect(sell_rect.left - btn_w - padding, menu_rect.bottom - btn_h - padding, btn_w, btn_h)
 
     pygame.draw.rect(screen, (4, 207, 116), buy_rect)
-
-
     draw_animated_text(
-        screen,
-        button_text_buy,
-        fonts["small"],
-        (255, 255, 255),
-        (menu_rect.centerx, buy_center_y),
-        "buy_button"
+        screen, 
+        "BUY", 
+        fonts["small"], 
+        (255, 255, 255), 
+        (buy_rect.centerx, buy_rect.centery), "buy_button"
     )
 
-    sell_center_y = menu_rect.bottom - sy(80) + fonts["small"].get_height() // 2
-    sell_rect = pygame.Rect(menu_rect.centerx + sx(200), sell_center_y - sy(20), sx(200), sy(40))
-
-    button_text_sell = f"SELL"
-
-    pygame.draw.rect(screen, (4, 207, 116), sell_rect)
-
-
+    pygame.draw.rect(screen, (207, 4, 4), sell_rect)
     draw_animated_text(
-        screen,
-        button_text_sell,
-        fonts["small"],
-        (255, 255, 255),
-        (menu_rect.centerx, sell_center_y),
-        "sell_button"
+        screen, 
+        "SELL", 
+        fonts["small"], 
+        (255, 255, 255), 
+        (sell_rect.centerx, sell_rect.centery), "sell_button"
     )
     
     clickable_rects.append((buy_rect, "buy_button"))
